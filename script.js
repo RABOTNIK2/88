921c4cac8c2ee15b218a86757dd6d2679aa79945
jopa2288 <jakkda@com>
2022-12-10 14:06:07 +0300

    notcool

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let red=document.querySelector(".jopa1")
 let blue=document.querySelector(".jopa2")
 let green=document.querySelector(".jopa3")
+let bad=document.querySelector(".jopa4")
 let jopa=document.querySelector("body")
 function smena1(){
     jopa.style.backgroundColor="red"
@@ -14,3 +15,7 @@ function smena3(){
 red.addEventListener("click",smena1)
 blue.addEventListener("click",smena2)
 green.addEventListener("click",smena3)
+function smena4(){
+    jopa.style.backgroundColor="crimson"
+}
+bad.addEventListener("click",smena4)
