39ced2601ea4acb311085b684d9846e9e4794c95
jopa2288 <jakkda@com>
2022-12-10 13:43:29 +0300

    cool

---
+++ script.js
@@ -0,0 +1,16 @@
+let red=document.querySelector(".jopa1")
+let blue=document.querySelector(".jopa2")
+let green=document.querySelector(".jopa3")
+let jopa=document.querySelector("body")
+function smena1(){
+    jopa.style.backgroundColor="red"
+}
+function smena2(){
+    jopa.style.backgroundColor="blue"
+}
+function smena3(){
+    jopa.style.backgroundColor="green"
+}
+red.addEventListener("click",smena1)
+blue.addEventListener("click",smena2)
+green.addEventListener("click",smena3)
