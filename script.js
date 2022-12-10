1072d746eb69f6c70a8dbf42ab085d2bb833ed0e
sk1n1ck <119128077+sk1n1ck@users.noreply.github.com>
2022-12-10 15:30:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let green=document.querySelector(".jopa3")
 let bad=document.querySelector(".jopa4")
 let jopa=document.querySelector("body")
 function smena1(){
-    jopa.style.backgroundColor="red"
+    jopa.style.backgroundColor="#3caa3c"
 }
 function smena2(){
-    jopa.style.backgroundColor="blue"
+    jopa.style.backgroundColor="#c154c1"
 }
 function smena3(){
-    jopa.style.backgroundColor="green"
+    jopa.style.backgroundColor="#ffa420"
 }
 red.addEventListener("click",smena1)
 blue.addEventListener("click",smena2)
 green.addEventListener("click",smena3)
 function smena4(){
-    jopa.style.backgroundColor="crimson"
+    jopa.style.backgroundColor="#ca2c92"
 }
 bad.addEventListener("click",smena4)
